1c78eda65923f376988a53839ad39c847c6b7bef
Eventlistener <chanfcsg334@gmail.com>
2024-11-04 22:59:32 +0700

    add contructor

--- src/con1.java
+++ src/con1.java
@@ -1,9 +1,21 @@
+import java.util.Objects;
+
 public class con1 {
     private String _name;
     private String _membership;
     con1(String name , String membership){
         set_name(name);
         set_membership(membership);
+    }
+    public String toString(){
+        return get_name() + " " + get_membership();
+    }
+    con1(){
+
+    }
+
+    public boolean equals(con1 u2) {
+        return Objects.equals(get_name(), u2.get_name()) && Objects.equals(get_membership(), u2.get_membership());
     }
     // Get name method
     String get_name(){
--- src/main2.java
+++ src/main2.java
@@ -1,8 +1,9 @@
 public class main2 {
     public static void main(String[] args) {
-        con1 user = new con1("chantha","bro code");
-        System.out.println(user.get_name());
-        System.out.println(user.get_membership());
+        con1 u = new con1("chantha","bro code");
+        con1 u2 = new con1("chantha","bro code");
+        System.out.println(u.equals(u2));
+        System.out.println(u2);
 
     }
 
